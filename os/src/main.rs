5cb772cb6bad16cb9a6e988d568f16e5bba8e4dc
ZephyrusZhang <2996362441@qq.com>
2023-03-09 15:56:23 +0800

    Initial commit

---
+++ os/src/main.rs
@@ -0,0 +1,21 @@
+#![no_std]
+#![no_main]
+
+mod lang_items;
+mod sbi;
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
